cd02e3ccef2d7edee2cf97f2d75b7800396971e1
Taumi62831853071 <113852275+Taumi62831853071@users.noreply.github.com>
2022-10-23 20:48:07 +0300

    Add files via upload

---
+++ homework_methods/src/Main.java
@@ -0,0 +1,43 @@
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void isItALeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високостным");
+        } else System.out.println(year + " год не является високостным");
+    }
+    public static void appVersion(int clientDeviceYear, boolean clientOS) {
+
+        int currentYear = LocalDate.now().getYear();
+        if (clientDeviceYear < currentYear) {
+            if (clientOS == true) {
+                System.out.println("Установите версию облегченную приложения для Android по ссылке: ");
+            } else System.out.println("Установите облегченную версию приложения для iOS по ссылке: ");
+
+        } else if (clientOS == true) {
+            System.out.println("Установите версию приложения для Android по ссылке: ");
+        } else System.out.println("Установите версию приложения для iOS по ссылке: ");
+    }
+    public static void requiredDays(int deliveryDistance) {
+
+        int oneDay = 40;
+        int requiredDays = (deliveryDistance - 20) / oneDay + 1;
+        System.out.println("Потребуется дней: " + requiredDays);
+
+    }
+
+    public static void main(String[] args) {
+
+        int year = 2021;
+        isItALeapYear(year);
+
+        boolean clientOS = false;
+        int clientDeviceYear = 2020;
+        appVersion(clientDeviceYear, clientOS);
+
+        int deliveryDistance = 95;
+        requiredDays(deliveryDistance);
+
+    }
+}
